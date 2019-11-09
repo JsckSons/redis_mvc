738d9318434db264871d73ca76db5780b40419f1
haitaoscott <593081343@qq.com>
2019-11-09 18:53:02 +0800

    初始化导入

--- src/test/java/com/atguigu/test/RedisTemplateTest.java
+++ src/test/java/com/atguigu/test/RedisTemplateTest.java
@@ -1,10 +1,5 @@
 package com.atguigu.test;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,57 +7,63 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring/applicationContext-redis.xml")
 public class RedisTemplateTest {
 	@Autowired
 	RedisTemplate<String, String> redisTemplate;
-	
+
 	@Test
 	public void testValue() {
 		redisTemplate.boundValueOps("name").set("JackSon");
-	}	
+	}
 	@Test
 	public void getValue() {
 		String string = redisTemplate.boundValueOps("name").get();
 		System.out.println(string);
-	}	
+	}
 	@Test
 	public void testSet() {
 		redisTemplate.delete("nameSet");
-		redisTemplate.boundSetOps("nameSet").add("²Ü²Ù","Áõ±¸","ËïÈ¨","Öî¸ğÁÁ","ÕÔÔÆ");
+		redisTemplate.boundSetOps("nameSet").add("æ›¹æ“","åˆ˜å¤‡","å­™æƒ","è¯¸è‘›äº®","èµµäº‘");
 		Set<String> members = redisTemplate.boundSetOps("nameSet").members();
 		System.out.println(members);
 
-	}	
+	}
 	@Test
 	public void getSet() {
 		Set<String> members = redisTemplate.boundSetOps("nameSet").members();
 		System.out.println(members);
-		redisTemplate.boundSetOps("nameSet").remove("ÕÔÔÆ");
+		redisTemplate.boundSetOps("nameSet").remove("èµµäº‘");
 		members = redisTemplate.boundSetOps("nameSet").members();
 		System.out.println(members);
-	}	
-	
+	}
+
 	@Test
 	public void putList() {
 		redisTemplate.delete("nameList");
-		Long leftPushAll = redisTemplate.boundListOps("nameList").leftPushAll("²Ü²Ù","Áõ±¸","ËïÈ¨","Öî¸ğÁÁ","ÕÔÔÆ");
+		Long leftPushAll = redisTemplate.boundListOps("nameList").leftPushAll("æ›¹æ“","åˆ˜å¤‡","å­™æƒ","è¯¸è‘›äº®","èµµäº‘");
 		System.out.println(leftPushAll);
 		List<String> list = redisTemplate.boundListOps("nameList").range(0, 10);
 		System.out.println(list);
-	}	
+
+	}
 	@Test
 	public void putHash() {
 		Map<String,String> map = new HashMap<String,String>();
-		map.put("A", "²Ü²Ù");//("²Ü²Ù","Áõ±¸","ËïÈ¨","Öî¸ğÁÁ","ÕÔÔÆ")
-		map.put("B", "Áõ±¸");
-		map.put("C", "ËïÈ¨");
-		map.put("D", "Öî¸ğÁÁ");
+		map.put("A", "æ›¹æ“");//("æ›¹æ“","åˆ˜å¤‡","å­™æƒ","è¯¸è‘›äº®","èµµäº‘")
+		map.put("B", "åˆ˜å¤‡");
+		map.put("C", "å­™æƒ");
+		map.put("D", "è¯¸è‘›äº®");
 		redisTemplate.boundHashOps("nameHash").putAll(map);
 		Set<Object> keys = redisTemplate.boundHashOps("nameHash").keys();
 		for (Object key : keys) {
 			System.out.println(redisTemplate.boundHashOps("nameHash").get(key));
 		}
-	}	
+	}
 }
